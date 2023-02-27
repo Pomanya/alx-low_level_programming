d8af6bc921a04e41665b58cc9a6166886cd4c11f
Pomanya <patoomanya@gmail.com>
2023-02-27 01:31:57 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i, n;
+	int t1 = 0;
+	int t2 = 1;
+	int nextTerm = t1 + t2;
+
+	for (i = 3; i <= 50; i++)
+	{
+		printf("%d, ", nextTerm);
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+	}
+	return (0);
+}
