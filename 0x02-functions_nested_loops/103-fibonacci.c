21c736b597f4c39f4e6f171eeb1bb794b3e7410a
Pomanya <patoomanya@gmail.com>
2023-02-27 03:30:09 -0800

    fibonacci1

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	int t1 = 1;
+	int t2;
+	int sum = 0;
+
+	while (i <= 4000000)
+	{
+		printf("%d\n", i);
+		sum += i;
+		t2 = i + t1;
+		i = t1;
+		t1 = t2;
+	}
+	printf("\n%d", sum);
+	return (0);
+}
